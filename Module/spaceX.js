dab6e8aeca2bc6aa569cc60d762482bd4c591eb3
gacuchac <gabriel.cuchacovich@gmail.com>
2020-10-01 12:32:46 -0300

    Top 10 Bacteria Bar Chart

--- Module/spaceX.js
+++ Module/spaceX.js
@@ -9,16 +9,16 @@
 // console.log("using map")
 // d3.json(url).then(result => result.map( launch => console.log(launch.location.latitude)))
 
-// d3.json("samples.json").then(data => console.log(data));
-// d3.json("samples.json").then(data => data.metadata.map(sample => console.log(sample.wfreq)));
+// d3.json("static/json/samples.json").then(data => console.log(data));
+// d3.json("static/json/samples.json").then(data => data.metadata.map(sample => console.log(sample.wfreq)));
 
-// d3.json("samples.json").then(function(data){
+// d3.json("static/json/samples.json").then(function(data){
 //     wfreq = data.metadata.map(person => person.wfreq).sort((a,b) => b - a);
 //     filteredWfreq = wfreq.filter(element => element !=null);
 //     console.log(filteredWfreq);
 // });
 
-d3.json("samples.json").then(function(data){
+d3.json("static/json/samples.json").then(function(data){
     firstPerson = data.metadata[0];
     Object.entries(firstPerson).forEach(([key, value]) =>
       {console.log(key + ': ' + value);});
